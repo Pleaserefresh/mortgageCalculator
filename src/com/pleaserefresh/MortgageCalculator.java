eb95fe66e74d7a16d7ed18e10914cf479cba8d6e
Jacek Kudlanski <jacekkudlanski@gmail.com>
2021-08-26 18:50:12 +0200

    update 4

--- src/com/pleaserefresh/MortgageCalculator.java
+++ src/com/pleaserefresh/MortgageCalculator.java
@@ -1,19 +1,24 @@
 package com.pleaserefresh;
 
 public class MortgageCalculator {
+    public final static byte MONTHS_IN_YEAR = 12;
+    public final static byte PERCENT = 100;
     private int principal;
     private float annualInterest;
     private byte years;
-
     public MortgageCalculator(int principal, float annualInterest, byte years) {
         this.principal = principal;
         this.annualInterest = annualInterest;
         this.years = years;
     }
 
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
+    }
+
     public double calculateBalance(short numberOfPaymentsMade) {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -24,8 +29,8 @@ public class MortgageCalculator {
 
     public double calculateMortgage() {
 
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
